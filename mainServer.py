33e8ac01356428af9b1591ad02c412b2075ea099
ganeshsrv-kaboon <ganeshsrv32@gmail.com>
2024-01-07 23:29:51 +0530

    Super sampled

--- mainServer.py
+++ mainServer.py
@@ -19,33 +19,13 @@ cache = TTLCache(maxsize=1000, ttl=300)
 serverApp.config['JWT_SECRET_KEY'] = 'hrrr-weather-lawn'
 jwt = JWTManager(serverApp)
 
-
-class ChunkIdFinder:
-    fs = s3fs.S3FileSystem(anon=True)
-    chunk_index = xr.open_zarr(s3fs.S3Map("s3://hrrrzarr/grid/HRRR_chunk_index.zarr", s3=fs))
-
-    @classmethod
-    def getChunkId(cls, lat, long):
-     projection = ccrs.LambertConformal(central_longitude=262.5, 
-                                        central_latitude=38.5, 
-                                        standard_parallels=(38.5, 38.5),
-                                        globe=ccrs.Globe(semimajor_axis=6371229, semiminor_axis=6371229))
-     x, y = projection.transform_point(long, lat, ccrs.PlateCarree())
-    #  pprint(f"x is :{x} and y is {y}")
-     nearest_point = cls.chunk_index.sel(x=x, y=y, method="nearest")
-     fcst_chunk_id = nearest_point.chunk_id.values
-     return fcst_chunk_id, nearest_point
-    
-
-# class ChunkIdFinderV2:
-#     chunk_index = xr.open_zarr("/grid/HRRR_chunk_index.zarr")
-#     ds_transposed = chunk_index['chunk_id'].transpose('x', 'y')
-#     chunk_index['chunk_id']= ds_transposed
+# CHUNK INDEX VERSION 1
+# class ChunkIdFinder:
+#     fs = s3fs.S3FileSystem(anon=True)
+#     chunk_index = xr.open_zarr(s3fs.S3Map("s3://hrrrzarr/grid/HRRR_chunk_index.zarr", s3=fs))
 
 #     @classmethod
 #     def getChunkId(cls, lat, long):
-#     #  pprint(f'Latsrv: {lat}, Long: {long}')  # Print lat and long for debugging
-#     #  pprint(cls.chunk_index)  # Print the chunk_index dataset for debugging
 #      projection = ccrs.LambertConformal(central_longitude=262.5, 
 #                                         central_latitude=38.5, 
 #                                         standard_parallels=(38.5, 38.5),
@@ -55,15 +35,35 @@ class ChunkIdFinder:
 #      nearest_point = cls.chunk_index.sel(x=x, y=y, method="nearest")
 #      fcst_chunk_id = nearest_point.chunk_id.values
 #      return fcst_chunk_id, nearest_point
+    
 
-@serverApp.before_request
-def validate_request():
-    required_fields = ['lat', 'long']  # replace with your actual fields
-    if not request.json:
-        return jsonify({'error': 'Missing JSON in request body'}), 400
-    for field in required_fields:
-        if field not in request.json:
-            return jsonify({'error': f'Missing field: {field}'}), 400
+# CHUNK INDEX AFTER SUPERSAMPLING TO 1KM RESOLUTION
+class ChunkIdFinderV2:
+    chunk_index = xr.open_zarr("../US-grid/HRRR_chunk_index.zarr")
+    # ds_transposed = chunk_index['chunk_id'].transpose('x', 'y')
+    # chunk_index['chunk_id']= ds_transposed
+
+    @classmethod
+    def getChunkId(cls, lat, long):
+     projection = ccrs.LambertConformal(central_longitude=262.5, 
+                                        central_latitude=38.5, 
+                                        standard_parallels=(38.5, 38.5),
+                                        globe=ccrs.Globe(semimajor_axis=6371229, semiminor_axis=6371229))
+     x, y = projection.transform_point(long, lat, ccrs.PlateCarree())
+     nearest_point = cls.chunk_index.sel(x=x, y=y, method="nearest")
+     fcst_chunk_id = nearest_point.chunk_id.values
+     return fcst_chunk_id, nearest_point
+
+# @serverApp.before_request
+# def validate_request():
+#     if request.endpoint == '/health':
+#         return 
+#     required_fields = ['lat', 'long'] 
+#     if not request.json:
+#         return jsonify({'error': 'Missingyu JSON in request body'}), 400
+#     for field in required_fields:
+#         if field not in request.json:
+#             return jsonify({'error': f'Missing field: {field}'}), 400
 
 # Test request
 @serverApp.route('/health')
@@ -117,19 +117,20 @@ def get_latest_folder(relative_path):
 
 
 def retrieve_data_local(url):
+    print(url)
     with open(url, 'rb') as compressed_data: # using regular file system
         buffer = ncd.blosc.decompress(compressed_data.read())
         # pprint(buffer)
 
-        dtype = "<f4"
+        dtype = "<f2"
         if "surface/PRES" in url: # surface/PRES is the only variable with a larger data type
             dtype = "<f4"
         # print(dtype)
-        chunk = np.frombuffer(buffer, dtype)
-        
+        chunk = np.frombuffer(buffer)
+        # print(chunk)
         entry_size = 150*150
         num_entries = len(chunk)//entry_size
-
+        print(num_entries)
         if num_entries == 1: # analysis file is 2d
             data_array = np.reshape(chunk, (150, 150))
         else:
@@ -150,10 +151,25 @@ def getTemperatureChunk():
     pprint(data)
     lat = data['lat']
     long = data['long']
-    chunk_id_finder = ChunkIdFinder()
+    chunk_id_finder = ChunkIdFinderV2()
     chunk_id, nearest_point = chunk_id_finder.getChunkId(lat, long)
+    print(chunk_id)
     array = getChunkArr(chunk_id,'t2m')
     return jsonify({'chunk': array.tolist()})
+
+
+# @serverApp.route('/temperature/now/chunk/v2', methods=['POST'])
+# # @jwt_required()
+# def getTemperatureChunk1():
+#     data = request.get_json()
+#     pprint(data)
+#     lat = data['lat']
+#     long = data['long']
+#     chunk_id_finder = ChunkIdFinderV2()
+#     chunk_id, nearest_point = chunk_id_finder.getChunkId(lat, long)
+#     print(chunk_id)
+#     array = getChunkArr(chunk_id,'t2m')
+#     return jsonify({'chunk': array.tolist()})
 
 @cached(cache)
 @serverApp.route('/visibility/now/chunk', methods=['POST'])
@@ -163,7 +179,7 @@ def getVisibilityChunk():
     pprint(data)
     lat = data['lat']
     long = data['long']
-    chunk_id_finder = ChunkIdFinder()
+    chunk_id_finder = ChunkIdFinderV2()
     chunk_id, nearest_point = chunk_id_finder.getChunkId(lat, long)
     # chunk_id, nearest_point = getChunkId(lat,long)
     # pprint(str(chunk_id))
@@ -173,12 +189,13 @@ def getVisibilityChunk():
 
 
 @serverApp.route('/temperature/now', methods=['POST'])
-@jwt_required()
+# @jwt_required()
 def getTemperature():
     data = request.get_json()
     lat = data['lat']
     long = data['long']
-    chunk_id_finder = ChunkIdFinder()
+    # chunk_id_finder = ChunkIdFinder()
+    chunk_id_finder = ChunkIdFinderV2()
     chunk_id, nearest_point = chunk_id_finder.getChunkId(lat, long)
     temperature = getChunk(chunk_id,nearest_point,'t2m')
     tempF = kelvin_to_fahrenheit(temperature)
@@ -207,7 +224,7 @@ def getVisibility():
     pprint(data)
     lat = data['lat']
     long = data['long']
-    chunk_id_finder = ChunkIdFinder()
+    chunk_id_finder = ChunkIdFinderV2()
     chunk_id, nearest_point = chunk_id_finder.getChunkId(lat, long)
     visibility = getChunk(chunk_id,nearest_point,'vis')
     serialized_visibility = float(visibility)  # Convert to a float
